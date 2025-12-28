bf74e030b610a71db5fa6c9f765a33ce0bf6c4a9
Your Name <your-email@example.com>
2025-12-28 14:44:34 +0200

    chore: adding docker compose for the db

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,6 +1,6 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import PostgresDsn,computed_field
 from typing import Literal
-
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".envs/.env.local", env_ignore_empty=True,extra="ignore")
@@ -10,6 +10,15 @@ class Settings(BaseSettings):
     SITE_NAME: str=""
     ENVIRONMENT: Literal["local", "staging", "production"]="local"
     DEBUG:bool=False
+    # Postgres Settings
+    POSTGRES_USER_NAME:str=""
+    POSTGRES_PASSWORD:str=""
+    POSTGRES_HOST:str=""
+    POSTGRES_PORT:int=int("")
+    @computed_field
+    @property
+    def POSTGRES_URL(self)->PostgresDsn:
+       return PostgresDsn.build(scheme="postgresql+asyncpg",host=self.POSTGRES_HOST,port=self.POSTGRES_PORT,username=self.POSTGRES_USER_NAME,password=self.POSTGRES_PASSWORD,path="nextgen")
 
 
 settings=Settings()
